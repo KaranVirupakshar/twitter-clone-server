aa89e9a4b89db7a25650c037fb0c35e60f655ed2
KaranVirupakshar <karan.virupakshar@gmail.com>
2021-11-25 18:28:12 -0500

    web-dev-node version 8

--- services/profile-service.js
+++ services/profile-service.js
@@ -6,47 +6,12 @@ module.exports = (app) => {
         res.json(profile);
     }
 
-    /*
-    const updateCurrentProfile = (req, res) => {
-        //  console.log("asdas"+req.profile.name)
-        console.log("req N "+req.body.name)
-        console.log("req L "+req.body.location)
-        console.log("pro fn " + profile['firstName'] )
-
-        if (profile['firstName']) {
-            profile['firstName'] = req.body.name
-        }
-        if (profile['bio']) {
-            profile['bio'] = req.body.bio
-        }
-        if (profile['dateOfBirth']) {
-            profile['dateOfBirth'] = req.body.dateOfBirth
-        }
-        if (profile['location']) {
-            profile['location'] = req.body.location
-        }
-        if (profile['website']) {
-            profile['website'] = req.body.website
-        }
-
-        console.log("pro fn " + profile['firstName'] )
-        console.log("req N "+req.body.name)
-        console.log("req L "+req.body.location)
-
-        return {
-            profile
-        };
-        res.json(profile);
-    }
-
-     */
 
     const updateCurrentProfile = (req, res) => {
         const editedProfile = req.body;
         for(const key in editedProfile){
             if(key == 'name'){
-                profile['firstName'] = editedProfile['name'].split(" ")[0];
-                profile['lastName'] = editedProfile['name'].split(" ")[1];
+                profile['firstName'] = editedProfile['name'];
             }
             else{
                 profile[key] = editedProfile[key];
