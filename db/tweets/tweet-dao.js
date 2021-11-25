e4c96d18b246274805b6c90fb81fed3ea6ffa661
KaranVirupakshar <karan.virupakshar@gmail.com>
2021-11-25 18:20:59 -0500

    web-dev-node version 7

---
+++ db/tweets/tweet-dao.js
@@ -0,0 +1,11 @@
+const model = require('tweets-model');
+
+const findAllTweets = () => {};
+const createTweet = (tweet) => {};
+const deleteTweet = (id) => {};
+const updateTweet = (id, tweet) => {};
+
+module.exports = {
+    findAllTweets, createTweet,
+    deleteTweet, updateTweet
+};
--- services/profile-service.js
+++ services/profile-service.js
@@ -6,36 +6,55 @@ module.exports = (app) => {
         res.json(profile);
     }
 
+    /*
     const updateCurrentProfile = (req, res) => {
-      //  console.log("asdas"+req.profile.name)
-
+        //  console.log("asdas"+req.profile.name)
         console.log("req N "+req.body.name)
         console.log("req L "+req.body.location)
-
         console.log("pro fn " + profile['firstName'] )
 
-        /*
-        profile['firstName'] = req.body.name
-        profile['bio'] = req.body.bio
-        profile['dateOfBirth'] = req.body.dateOfBirth
-        profile['location'] = req.body.location
-        profile['website'] = req.body.website
-
-         */
+        if (profile['firstName']) {
+            profile['firstName'] = req.body.name
+        }
+        if (profile['bio']) {
+            profile['bio'] = req.body.bio
+        }
+        if (profile['dateOfBirth']) {
+            profile['dateOfBirth'] = req.body.dateOfBirth
+        }
+        if (profile['location']) {
+            profile['location'] = req.body.location
+        }
+        if (profile['website']) {
+            profile['website'] = req.body.website
+        }
 
         console.log("pro fn " + profile['firstName'] )
-
         console.log("req N "+req.body.name)
         console.log("req L "+req.body.location)
-
 
         return {
-
             profile
-
         };
-        res.sendStatus(200);
+        res.json(profile);
     }
+
+     */
+
+    const updateCurrentProfile = (req, res) => {
+        const editedProfile = req.body;
+        for(const key in editedProfile){
+            if(key == 'name'){
+                profile['firstName'] = editedProfile['name'].split(" ")[0];
+                profile['lastName'] = editedProfile['name'].split(" ")[1];
+            }
+            else{
+                profile[key] = editedProfile[key];
+            }
+        }
+        res.json(profile);
+    }
+
     app.put('/api/profile', updateCurrentProfile);
 
     app.get('/api/profile', getCurrentProfile);
